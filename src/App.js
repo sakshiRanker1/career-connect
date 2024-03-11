213394257aa9566062f91ab9868bbd6ec5a035ba
Sakshi Jha <129147399+sakshiRanker1@users.noreply.github.com>
2024-03-11 13:34:25 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import "./App.css";
-import Home from "./components/Home";
-import Jobs from "./components/Jobs";
+import './App.css';
+import Home from './components/Home';
+import Jobs from './components/Jobs';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import PostJob from "./components/PostJob";
-import SaveJobs from "./components/SaveJobs";
-import Discussion from "./components/Discussion";
-import ErrorPage from "./components/ErrorPage";
-import ApplyJobs from "./components/ApplyJobs";
+import PostJob from './components/PostJob';
+import SaveJobs from './components/SaveJobs';
+import Discussion from './components/Discussion';
+import ErrorPage from './components/ErrorPage';
+import ApplyJobs from './components/ApplyJobs';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/career-connect/">
+          <Route path="/">
             <Route index element={<Home />} />
-            <Route path="/career-connect/home" element={<Home />} />
-            <Route path="/career-connect/jobs" element={<Jobs />} />
-            <Route path="/career-connect/post-job" element={<PostJob />} />
-            <Route path="/career-connect/apply-jobs" element={<ApplyJobs />} />
-            <Route path="/career-connect/saved-job" element={<SaveJobs />} />
-            <Route path="/career-connect/discussion" element={<Discussion />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/jobs" element={<Jobs />} />
+            <Route path="/post-job" element={<PostJob />} />
+            <Route path="/apply-jobs" element={<ApplyJobs />} />
+            <Route path="/saved-job" element={<SaveJobs />} />
+            <Route path="/discussion" element={<Discussion />} />
             <Route path="*" element={<ErrorPage />} />
           </Route>
         </Routes>
